a9d71f95259e1966728ac5b5650c69926abdb59d
Gabriel <XyPhoGR@gmail.com>
2017-11-09 18:36:29 -0600

    working on brute-force solver

--- solver.py
+++ solver.py
@@ -1,4 +1,6 @@
-import board
+import board, queue
+
+Queue = queue.Queue
 
 class Solver:
     def __init__(self, board, depth):
@@ -10,9 +12,22 @@ class Solver:
     #clears the cache of decided moves
     #eg, if the user has changed the board's state
     def reset(self):
-        self.q = [] # queue of moves
+        self.q = Queue() # queue of moves
         pass
 
     #returns the next move in the queue
     def move(self):
+        q = self.q
+        if q.empty():
+            #pass fields as args for convenience
+            self.generate(q, self.board, self.depth)
+        #just in case...
+        if q.empty():
+            return None
+        return q.get()
+
+    def generate(self, q, board, depth):
+        #put crap into queue
+        #basically exponentially search for best solution up to a set depth
+        #TODO: maybe do it better but probs not huehuehue
         pass
